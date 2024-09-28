18d51ab6253566b549a6c5780fdf0f6e273cfd07
DurairajGopal <67893045+DurairajGopal@users.noreply.github.com>
2024-09-28 15:12:37 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -103,12 +103,12 @@ resource "aws_eks_node_group" "devopsshack" {
   subnet_ids      = aws_subnet.devopsshack_subnet[*].id
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
-  instance_types = ["t2.large"]
+  instance_types = ["t2.medium"]
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
